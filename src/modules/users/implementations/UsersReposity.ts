75b17dca027cd98532b18cd468de5555ccbe2a6e
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-21 00:16:14 +0000

    feat: implement findByEmail

--- src/modules/users/implementations/UsersReposity.ts
+++ src/modules/users/implementations/UsersReposity.ts
@@ -4,6 +4,15 @@ import { IUsersRepository } from "../repositories/IUsersRepository";
 import { prismaClient } from "../../../prisma";
 
 export class UsersRepository implements IUsersRepository {
+ async  findByEmail(email: string): Promise<User> {
+       const user = await prismaClient.user.findFirst({
+           where: {
+               email
+           }
+       });
+
+       return user as User;
+   }
    async create({ name, email, password }: ICreateUserDto): Promise<User> {
         const user = await prismaClient.user.create({
             data: {
--- src/modules/users/useCases/createUser/createUserUseCases.ts
+++ src/modules/users/useCases/createUser/createUserUseCases.ts
@@ -1,7 +1,12 @@
+import { ICreateUserDto } from "../../dtos/ICreateUsersDtos";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 export class CreateUserUseCases {
   constructor(
     private usersRepository:IUsersRepository
   ){}
+
+  execute({ name, email, password }: ICreateUserDto) {
+    const verifyIfUserAlreadyExists = this.usersRepository.findByEmail(email);
+  }
 }
