2be2b9966127cc956424ab64e6541ed8dd044b93
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-21 17:00:15 +0000

    feat: createMoviesController

---
+++ src/modules/movies/useCases/createMovies/createMoviesControllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateMoviesUseCases } from "./createMoviesUseCases";
+
+export class CreateMoviesControllers {
+    async handle(req: Request, res: Response) {
+        const {name, description, daily_rate, fine_amount, available, genre_id} = req.body;
+
+        const createMoviesUseCases = container.resolve(CreateMoviesUseCases);
+
+        const movies = await createMoviesUseCases.execute({
+            available,
+            daily_rate,
+            description,
+            fine_amount,
+            genre_id,
+            name
+        })
+
+        return res.status(201).json(movies);
+}
+}
