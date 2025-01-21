1158fba705977753de417e923089f85736bd5b97
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-21 03:31:51 +0000

    feat: create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { hash } from "bcrypt";
+import { prismaClient } from "../src/prisma";
+
+async function main() {
+    const passwordHash = await hash("admin", 8);
+
+    const user = {
+        name: "admin",
+        email: "admin@movies.com",
+        isAdmin: true,
+        password: passwordHash
+    }
+
+    await prismaClient.user.create({ data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin
+    } });
+}
+
+main();
