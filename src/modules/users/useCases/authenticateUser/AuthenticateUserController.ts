5318efeb7e216547b6b6c4725250e1373a0d7aa4
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-21 02:24:18 +0000

    feat: create authenticateUserController

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.ts
@@ -1,3 +1,16 @@
+
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
 export class AuthenticateUserController{
-    
+  async handle(req:Request, res:Response) {
+    const {email,password} = req.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const tokenResponse = await authenticateUserUseCase.execute({email,password});
+
+    return res.status(200).json(tokenResponse);
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/users/useCases/createUser/createUserController";
 import asyncHandler from "../helpers/routes/exception/controllException";
+import { AuthenticateUserController } from "../modules/users/useCases/authenticateUser/AuthenticateUserController";
+
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
+
 export const userRouter = Router();
 
 userRouter.post('/', asyncHandler(createUserController.handle));
-
+userRouter.post('/session', asyncHandler(authenticateUserController.handle));
 
 
