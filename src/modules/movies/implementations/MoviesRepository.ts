c5f366560cb91e34dc2e3ffdb556ebaa00d784ed
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-21 17:09:41 +0000

    feat: add list , findById methods

--- src/modules/movies/implementations/MoviesRepository.ts
+++ src/modules/movies/implementations/MoviesRepository.ts
@@ -4,6 +4,18 @@ import { IMoviesRepository } from "../repositories/IMoviesRepository";
 import { prismaClient } from "../../../prisma";
 
 export class MoviesRepository implements IMoviesRepository {
+  listAll(): Promise<Movie[]> {
+      const movies = prismaClient.movie.findMany();
+      return movies as Promise<Movie[]>
+  }
+  findById(id: string): Promise<Movie> {
+      const movie = prismaClient.movie.findUnique({
+          where: {
+              id
+          }
+      })
+      return movie as Promise<Movie>
+  }
   async  create({ name, description, daily_rate, fine_amount, available, genre_id }: ICreateMoviesDtos): Promise<Movie> {
         const movie = await prismaClient.movie.create({
             data: {
--- src/modules/movies/repositories/IMoviesRepository.ts
+++ src/modules/movies/repositories/IMoviesRepository.ts
@@ -4,4 +4,6 @@ import { ICreateMoviesDtos } from "../dtos/ICreateMoviesDtos"
 export interface IMoviesRepository {
     create({name, description, daily_rate, fine_amount, available, genre_id}: ICreateMoviesDtos): Promise<Movie>
     findByName(name: string): Promise<Movie>
+    listAll(): Promise<Movie[]>
+    findById(id: string): Promise<Movie>
 }
